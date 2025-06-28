53ef998eb2b711dc9b062c92c21cc2b5b18035db
pinguin <--global>
2025-06-29 01:41:09 +0200

    allowed image origin from own cdn

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        pathname: '/eulesstack/shopora/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
