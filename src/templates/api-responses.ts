521b2ecd687dddc9a5849f202e6006100f4f7b64
pinguin <noah@eule.fritz.box>
2025-06-28 21:36:33 +0200

    templated api responses to preventredundancy

---
+++ src/templates/api-responses.ts
@@ -0,0 +1,57 @@
+const APIResponseTemplate = {
+    ok: (message: string = "Success") => ({
+        success: true,
+        data: null,
+        message,
+        status: 200,
+    }),
+
+    //on POST/Insert operations
+    created: (data?: unknown, message: string = "Resource created") => ({
+        success: true,
+        data,
+        message,
+        status: 201,
+    }),
+
+    badRequest: (data?: unknown, message: string = "Bad Request") => ({
+        success: false,
+        data,
+        message,
+        status: 400,
+    }),
+
+    //not authorized
+    unauthorized: (message = "Unauthorized") => ({
+        success: false,
+        data: null,
+        message,
+        status: 401,
+    }),
+
+    //authenticated but not authorized
+    forbidden: (message = "Forbidden") => ({
+        success: false,
+        data: null,
+        message,
+        status: 403,
+    }),
+
+    //not-locatable resource
+    notFound: (message = "Not found") => ({
+        success: false,
+        data: null,
+        message,
+        status: 404,
+    }),
+    //dynamic server error
+    internalServerError: (error: Error | string) => ({
+        success: false,
+        data: null,
+        message: "Internal server error",
+        error: error,
+        status: 404,
+    }),
+}
+
+export default APIResponseTemplate;
