eefb7bda8b5012a9fa5c34325e11e58e2fb8ae78
root <--global>
2025-06-29 13:47:58 +0200

    fixed overflowing content

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,7 +8,7 @@ export default async function page() {
     const products = await database.product.findMany();
 
     return (
-        <div className="size-full">
+        <div className="flex-1 flex">
             <ProductList products={products} />
         </div>
     );
