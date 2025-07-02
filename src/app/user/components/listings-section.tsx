9c72d145c44378dd3f235bb3ab74da6ad9e73d00
noahSnbr07 <--global>
2025-07-02 13:53:58 +0200

    user profile splitted into 2 section

---
+++ src/app/user/components/listings-section.tsx
@@ -0,0 +1,20 @@
+'use server';
+
+import { ProductList } from "@/components/product-utils";
+import { Product } from "@prisma/client";
+
+interface _props {
+    name: string;
+    listed: Product[];
+}
+
+export default async function ListingsSection({ name, listed }: _props) {
+    return (
+        <div className="flex-1 p-4 flex flex-col gap-4">
+            <b className="text-xl">{name}'s 10 recent Products</b>
+            <div className="flex-1 overflow-y-auto">
+                <ProductList products={listed} />
+            </div>
+        </div>
+    );
+}
