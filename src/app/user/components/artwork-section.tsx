22b6a3e55ebd1f1a2eb2fe85ba26c460aec894ae
noahSnbr07 <--global>
2025-07-02 13:25:54 +0200

    added online indicator

--- src/app/user/components/artwork-section.tsx
+++ src/app/user/components/artwork-section.tsx
@@ -13,14 +13,17 @@ export default async function ArtworkSection({ source, name, created, lastSignIn
     console.log({ source, name, created, lastSignIn })
     return (
         <div className="flex flex-col gap-4 p-4 border-r-2 border-stack h-full">
-            <Image
-                width={200}
-                height={200}
-                alt="User"
-                title="User"
-                src={source}
-                className="object-cover aspect-square rounded-full"
-            />
+            <div className="relative">
+                <Image
+                    width={200}
+                    height={200}
+                    alt="User"
+                    title="User"
+                    src={source}
+                    className="object-cover aspect-square rounded-full"
+                />
+                <div className="absolute bg-green-600 size-8 rounded-full bottom-4 right-4"></div>
+            </div>
             <b className="text-lg"> {name} </b>
             <div className="flex flex-col gap-1">
                 <i className="opacity-50"> joined: {created.toLocaleDateString()} </i>
